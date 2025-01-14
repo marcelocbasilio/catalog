b835fa8fb0746733e3f67f008673468f0f7b8675
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2025-01-14 13:10:06 -0300

    Database seeding.

--- src/main/java/com/marcelocbasilio/catalog/services/CategoryService.java
+++ src/main/java/com/marcelocbasilio/catalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.marcelocbasilio.catalog.services;
 import com.marcelocbasilio.catalog.entities.Category;
 import com.marcelocbasilio.catalog.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
